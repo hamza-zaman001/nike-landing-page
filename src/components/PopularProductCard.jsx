cc2ed3c3be242d93cf75cbbd643d472db8b8bca3
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-15 23:18:52 +0500

    .

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -9,7 +9,11 @@ const PopularProductCard = ({ imgURL, name, price}) => {
       <div className='mt-8 flex justify-start gap-2.5'>
         <img src={star} alt="rating" width={24}
         height={24} />
+        <p className='font-montserrat text-xl leading-normal
+        text-slate-gray'>(4.5)</p>
       </div>
+      <h3 className='mt-2 text-2xl leading-normal font-semibold'>{name}</h3>
+      <p>{price}</p>
     </div>
   )
 }
