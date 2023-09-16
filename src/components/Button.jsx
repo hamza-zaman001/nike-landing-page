d49a2d8a9754b79e38e5eca470b74b411d1a6d5e
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-16 20:29:02 +0500

    done w Special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className='flex justify-center items-center
-    gap-2 px-7 py-4 border font-montserrat text-lg
-    leading-none bg-coral-red rounded-full
-    text-white border-coral-red hover:scale-110 duration-500'>
-      {label} 
-      {iconURL && <img src={iconURL} alt="arrow-icon"
-      className='ml-2 rounded-full 
-      w-5 h-5' />}
+    <button 
+      className={`flex justify-center items-center
+      gap-2 px-7 py-4 border font-montserrat text-lg
+      leading-none hover:scale-110 duration-500
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red text-white border-coral-red'
+        } rounded-full w-full'}`}>
+        {label} 
+        {iconURL && <img src={iconURL} alt="arrow-icon"
+        className='ml-2 rounded-full 
+        w-5 h-5' />}
     </button>
   )
 }
